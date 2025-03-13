6546eec3ee495ba09b01b049e744fe530ebc2448
Hypery Music <alex@hypery.co>
2025-03-13 18:42:45 +0000

    fix

--- post-build.js
+++ post-build.js
@@ -134,6 +134,7 @@ function updateFileContent(filePath, metadata) {
 <meta name="description" content="${metadata.description}">
 <meta name="keywords" content="${metadata.keywords}">
 <meta name="author" content="CompressPictures.com">
+<meta name="google-adsense-account" content="ca-pub-4577146981460520">
 <meta property="og:title" content="${metadata.title}">
 <meta property="og:description" content="${metadata.description}">
 <meta property="og:type" content="website">
